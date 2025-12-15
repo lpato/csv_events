97722e5a8dce4b5d9056b2cca7f06bece7b6ad9b
lpato <lorenaspato@gmail.com>
2025-12-15 17:49:16 +0100

    feat:inital project setup

---
+++ src/main/java/com/lsp/csv_events/CsvEventsApplication.java
@@ -0,0 +1,13 @@
+package com.lsp.csv_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsp/csv_events/CsvEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsp.csv_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
